81ae7bf80639100a6a45362a83aa181a952536cb
tampe01 <tampewilliams89@gmail.com>
2020-01-21 09:09:07 +0000

    Add files via upload

---
+++ controllers/Home.php
@@ -0,0 +1,80 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Class Auth
+ * @property Ion_auth|Ion_auth_model $ion_auth        The ION Auth spark
+ * @property CI_Form_validation      $form_validation The form validation library
+ */
+class Home extends CI_Controller
+{
+	public $data = [];
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+		$this->load->library(['ion_auth', 'form_validation']);
+		$this->load->helper(['url', 'language']);
+	}
+	
+	public function index() {
+	
+		if (!$this->ion_auth->logged_in())
+		{
+			// redirect them to the login page
+			redirect('auth/login', 'refresh');
+		}
+		else if (!$this->ion_auth->is_admin()) // remove this elseif if you want to enable this for non-admins
+		{
+			// redirect them to the home page because they must be an administrator to view this
+			show_error('You must be an administrator to view this page.');
+		}
+		else
+		{
+			$this->data['title'] = $this->lang->line('index_heading');
+			
+			// set the flash data error message if there is one
+			$this->data['message'] = (validation_errors()) ? validation_errors() : $this->session->flashdata('message');
+
+			//list the users
+			$this->data['users'] = $this->ion_auth->users()->result();
+
+			$user = $this->ion_auth->user()->row();
+			$data['user'] = $user;
+			
+			//USAGE NOTE - you can do more complicated queries like this
+			//$this->data['users'] = $this->ion_auth->where('field', 'value')->users()->result();
+			
+			// foreach ($this->data['users'] as $k => $user)
+			// {
+				// $this->data['users'][$k]->groups = $this->ion_auth->get_users_groups($user->id)->result();
+			// }
+
+			$this->_render_page('' . DIRECTORY_SEPARATOR . 'index', $data);
+		}
+	}
+	
+	/**
+	 * @param string     $view
+	 * @param array|null $data
+	 * @param bool       $returnhtml
+	 *
+	 * @return mixed
+	 */
+	public function _render_page($view, $data = NULL, $returnhtml = FALSE)//I think this makes more sense
+	{
+
+		$viewdata = (empty($data)) ? $this->data : $data;
+
+		$view_html = $this->load->view($view, $viewdata, $returnhtml);
+
+		// This will return html on 3rd argument being true
+		if ($returnhtml)
+		{
+			return $view_html;
+		}
+	}
+	
+}
+
+?>
